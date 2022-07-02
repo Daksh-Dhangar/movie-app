d5c907b31fc116fc3707bafa81825dc6870f9276
dzksh <dakshdhangar@gmail.com>
2022-07-02 13:33:24 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully.");
+});
+
